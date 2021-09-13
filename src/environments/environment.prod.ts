bbbb939ef083b4b985b6ce67b01fe442190a671f
Viktor Pavlov <70695079+ViktorP1@users.noreply.github.com>
2021-09-13 16:02:45 +0200

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  api_url: 'https://cors-anywhere.herokuapp.com/https://api.edamam.com/api/recipes/v2/0123456789abcdef0123456789abcdef?app_id={app_id}&app_key={app_key}&type=public',
+  app_id: '62bc7ce4',
+  app_key: '492328fb6f4b9f8cfb88518217f44ea6',
+};
